7087217df6b20e98beefb1990aee9fd1a6d4d112
miguelnkono <nkonondememiguel7@gmail.com>
2024-06-30 00:11:21 +0200

    Finished the raw input feature.

--- kilo.c
+++ kilo.c
@@ -23,7 +23,12 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -33,7 +38,7 @@ struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
-    struct termios orig_termios;
+    struct termios orig_termios;  // For low-level stuff "Raw Mode"
 };
 
 struct editorConfig E;
@@ -68,8 +73,9 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+// Reading a key.
 int editorReadKey() {
-    int nread;
+    int nread; // numbers of bits of key read.
     char c;
     while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
@@ -82,11 +88,35 @@ int editorReadKey() {
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if (seq[0] == '[') {
+
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) 
+                    return '\x1b';
+                if (seq[2] == '~') {
+                    switch(seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch(seq[1]) {
+                    case 'A': return ARROW_UP;   // Up.
+                    case 'B': return ARROW_DOWN;   // Down.
+                    case 'C': return ARROW_RIGHT;   // Right.
+                    case 'D': return ARROW_LEFT;   // Left.
+                    case 'H': return HOME_KEY;  // Home key
+                    case 'F': return END_KEY;   // End key
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch(seq[1]) {
-                case 'A': return ARROW_UP;   // Up.
-                case 'B': return ARROW_DOWN;   // Down.
-                case 'C': return ARROW_RIGHT;   // Right.
-                case 'D': return ARROW_LEFT;   // Left.
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -258,6 +288,27 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(1);
+            break;
+
+            /* For now we simplt make the HOME_KEY and the
+             * END_KEY move the cursor left and right.*/
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+
+            /* For now PAGE_UP and PAGE_DOWN simply move the
+             * cursor to the top or the bottom of the page.*/
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while(times--)
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
             break;
 
         case ARROW_UP:
