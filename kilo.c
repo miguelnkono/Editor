0c026bd2b3af8a7bea0275f81a1222231c7a469f
miguelnkono <nkonondememiguel7@gmail.com>
2024-06-29 21:06:22 +0200

    Add some features like moving the cursor around

--- kilo.c
+++ kilo.c
@@ -14,12 +14,23 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+// Directions Keys.
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 /*** data ***/
 
 /// struct that is going to all our editor state.
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -57,13 +68,32 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch(seq[1]) {
+                case 'A': return ARROW_UP;   // Up.
+                case 'B': return ARROW_DOWN;   // Down.
+                case 'C': return ARROW_RIGHT;   // Right.
+                case 'D': return ARROW_LEFT;   // Left.
+            }
+        }
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -134,11 +164,29 @@ void abFree(struct abuf *ab)
 void editorDrawRows(struct abuf *ab) {
     int y;
     for(y = 0; y < E.screenrows; y++) {
-        //write(STDOUT_FILENO, "~", 1);
-        abAppend(ab, "~",  1);
+        //abAppend(ab, "~",  1);
+        
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), 
+                    "KILO editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else 
+        {
+            abAppend(ab, "~", 1);
+        }
 
+        //<esc>[K -> clear one line at the time.
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
-            //write(STDOUT_FILENO, "\r\n", 2);
             abAppend(ab, "\r\n",  2);
         }
     }
@@ -147,28 +195,91 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
-    //write(STDOUT_FILENO, "\x1b[2J", 4);
-    //write(STDOUT_FILENO, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[2J", 4);
+    /// (h, l) commands are used to turn on and turn off varius terminal features or "modes".
+    // So "l" it's for hiding and "h" for showing.
+    // We use here ?25l -> for hiding the cursor and
+    // ?25h -> to show it again.
+    abAppend(&ab, "\x1b[?25l", 6);
+    /*abAppend(&ab, "\x1b[2J", 4);*/  /// 2J -> clear the entire screen.
+                                  //<esc>[K -> clear one line at the time.
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
-    //write(STDOUT_FILENO, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[H", 3);
+    // Move the cursor to the position stored in E.cx and E.cy.
+    // \x1b[H is for the cursor.
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    //abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
 }
 
+/*** input ***/
+
+/// Allow the user to move the cursor around.
+void editorMoveCursor(int key) {
+    switch(key) {
+        case ARROW_LEFT:
+            if (E.cx != 0) {
+                E.cx--;
+            }
+            break;
+
+        case ARROW_RIGHT:
+            if (E.cx != E.screencols - 1){
+                E.cx++;
+            }
+            break;
+
+        case ARROW_UP:
+            if (E.cy != 0) {
+                E.cy--;
+            }
+            break;
+
+        case ARROW_DOWN:
+            if (E.cy != E.screenrows - 1) {
+                E.cy++;
+            }
+            break;
+    }
+}
+
+void editorProcessKeypress() {
+    int c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(1);
+            break;
+
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+    }
+}
+
 /*** init ***/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
 
-void editorProcessKeypress() {
+/*void editorProcessKeypress() {
     char c = editorReadKey();
 
     switch(c) {
@@ -178,7 +289,7 @@ void editorProcessKeypress() {
             exit(1);
             break;
     }
-}
+}*/
 
 int main() {
     enableRawMode();
